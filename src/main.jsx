638dc3a96a410c5dfc9e6cb8552bfca7f458ace3
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-01-23 19:51:02 +0200

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,25 @@ import App from "./components/App/App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { PersistGate } from "redux-persist/integration/react";
 // import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/prestige-drive" hashType="noslash">
-    <Provider store={store}>
-      <ToastContainer
-        position="top-right"
-        autoClose={1000}
-        closeOnClick
-        rtl={false}
-        theme="light"
-      />
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/prestige-drive">
+        <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={1000}
+          closeOnClick
+          rtl={false}
+          theme="light"
+        />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
